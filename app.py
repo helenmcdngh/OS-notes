00c6e478fa5a4b2ac034ffad61bdf36acc7658ac
Helen McDonagh <g00320304@gmit.ie>
2016-11-16 12:14:22 +0000

    8th

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import flask as fl
-from flask import render_template, request
+from flask import render_template, request, g
 import sqlite3
 #-----------------------------------------------------------#
 
@@ -14,12 +14,11 @@ def get_db():
     db = fl.g._database = sqlite3.connect(DATABASE)
   return db
 
-@app.teardown_appcontext
-def close_connection(exception):
-  db = getattr(fl.g, '_database', None)
-  if db is not None:
-    db.close()
-    
+#@app.teardown_appcontext
+#def close_connection(exception):
+  #db = getattr(fl.g, '_database', None)if db is not None:
+    #db.close()
+
 #-----------------------------------------------------------#
 #home page#
 @app.route("/")
@@ -30,16 +29,18 @@ def root():
 #inserting data into database#
 @app.route("/database", methods=["GET","POST"])
 def database():
+    #g.db = connect_db()
     cur = get_db().cursor()
-    #cur.execute("insert into mytable(title,define)"),(fl.request.form["title"],fl.request.form["define"])
+    #g.db.execute('insert into mytable(title,define) values (?,?)',[fl.request.form["title"],fl.request.form["define"]])
+    #g.db.commit()
     cur.execute("Select * from mytable")
-    return str(cur.fetchall()) + render_template('base.html')
+    return str(render_template('base.html') 
 
 #-----------------------------------------------------------#
 #first dummy info to page
-@app.route("/name", methods=["GET", "POST"])
-def name():
-    return fl.request.form["name"] + " : " + fl.request.form["comment"]
+#@app.route("/name", methods=["GET", "POST"])
+#def name():
+    #return fl.request.form["name"] + " : " + fl.request.form["comment"]
 
 #-----------------------------------------------------------#
 #information on page#
